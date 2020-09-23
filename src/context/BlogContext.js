1909ba98d1119d54e813e33de03c309c109f23dc
Adam Morgan <adamcodes716@gmail.com>
2020-09-23 10:23:08 -0400

    editing action function

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -30,8 +30,17 @@ const deleteBlogPost = dispatch => {
   };
 };
 
+const editBlogPost = dispatch => {
+    return (id, title, content) => {
+      dispatch({ 
+        type: 'edit_blogpost',
+         payload: { id, title, content}
+        });
+  };
+};
+
 export const { Context, Provider } = createDataContext(
   blogReducer,
-  { addBlogPost, deleteBlogPost },
+  { addBlogPost, deleteBlogPost, editBlogPost },
   [{ title: 'Test Post', content: 'Test Content', id: 1}]
 );
